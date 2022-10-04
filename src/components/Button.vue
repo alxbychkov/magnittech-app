8e7e52fcc13c3bfeaba8cf44a71f0f201148c528
Alexander Bychkov <alx.bychkov@gmail.com>
2022-10-04 19:37:44 +0300

    feature: init app

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <button></button>
+</template>
+
+<style scoped></style>
---
+++ src/views/TaskView.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Task number: {{ $route.params.id }}</h1>
+</template>
+
+<style></style>
